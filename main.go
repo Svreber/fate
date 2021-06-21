2555cc58001933ba7e5eff674c5bc5837b88a3ed
vthiery <vjmthiery@gmail.com>
2021-06-21 14:01:00 +0200

    Add flip and magic-ball endpoints
    Co-authored-by: nathsom

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -26,6 +27,8 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano())
 	e.GET("/roll", roll)
+	e.GET("/flip", flip)
+	e.GET("/magic-ball", magicBall)
 
 	// -- Start the server --
 
@@ -41,14 +44,64 @@ func health(c echo.Context) error {
 }
 
 func roll(c echo.Context) error {
-	i := rollDice6()
+	facesStr := c.QueryParam("faces")
+
+	if len(facesStr) == 0 {
+		facesStr = fmt.Sprint(6)
+	}
+
+	faces, err := strconv.Atoi(facesStr)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	i := rollDice(faces)
 
 	return c.String(http.StatusOK, fmt.Sprint(i))
 }
 
-func rollDice6() int {
-	i := rand.Intn(6)
+func rollDice(faces int) int {
+	i := rand.Intn(faces)
 	i++
 
 	return i
+}
+
+func flip(c echo.Context) error {
+	res := rollDice(2)
+
+	if res == 2 {
+		return c.String(http.StatusOK, "Head")
+	}
+
+	return c.String(http.StatusOK, "Tails")
+}
+
+var magicBallAnswers = []string{
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes definitely",
+	"You may rely on it",
+	"As I see it yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+}
+
+func magicBall(c echo.Context) error {
+	k := rand.Intn(len(magicBallAnswers))
+
+	return c.String(http.StatusOK, magicBallAnswers[k])
 }
--- main_test.go
+++ main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -13,7 +15,7 @@ import (
 
 func TestRoll(t *testing.T) {
 	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	req := httptest.NewRequest(http.MethodGet, "/roll", nil)
 
 	for i := 0; i < 10000; i++ {
 		rec := httptest.NewRecorder()
@@ -29,5 +31,73 @@ func TestRoll(t *testing.T) {
 
 		assert.GreaterOrEqual(t, res, 1)
 		assert.LessOrEqual(t, res, 6)
+	}
+}
+
+func TestRollWithArbitraryFaces(t *testing.T) {
+	e := echo.New()
+
+	n := rand.Int()
+	req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/roll?faces=%d", n), nil)
+
+	for i := 0; i < 10000; i++ {
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+
+		assert.NoError(t, roll(ctx))
+
+		payload, err := io.ReadAll(rec.Result().Body)
+		assert.NoError(t, err)
+
+		res, err := strconv.Atoi(string(payload))
+		assert.NoError(t, err)
+
+		assert.GreaterOrEqual(t, res, 1)
+		assert.LessOrEqual(t, res, n)
+	}
+}
+
+func TestRollDice(t *testing.T) {
+	n := rand.Int()
+
+	for i := 0; i < 10000; i++ {
+		res := rollDice(n)
+
+		assert.GreaterOrEqual(t, res, 1)
+		assert.LessOrEqual(t, res, n)
+	}
+}
+
+func TestFlip(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/flip", nil)
+
+	for i := 0; i < 10000; i++ {
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+
+		assert.NoError(t, flip(ctx))
+
+		payload, err := io.ReadAll(rec.Result().Body)
+		assert.NoError(t, err)
+
+		assert.Contains(t, []string{"Head", "Tails"}, string(payload))
+	}
+}
+
+func TestMagicBall(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/magic-ball", nil)
+
+	for i := 0; i < 10000; i++ {
+		rec := httptest.NewRecorder()
+		ctx := e.NewContext(req, rec)
+
+		assert.NoError(t, magicBall(ctx))
+
+		payload, err := io.ReadAll(rec.Result().Body)
+		assert.NoError(t, err)
+
+		assert.Contains(t, magicBallAnswers, string(payload))
 	}
 }
